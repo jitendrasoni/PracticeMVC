6dc3947e0a20d11b0a61e6d447ad00e6d8e63c36
jitendrasoni <jitusonijk@gmail.com>
2020-05-10 20:29:10 +1000

    LSP principle

---
+++ PracticeMVC/PracticeMVC/Console/Solid/LiskovSubstitutionPrinciple/LSP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    class Program
+    {
+        //The Liskov Substitution Principle(LSP) states that 
+        //you should be able to use any derived class instead of a parent class and have it behave" +
+        //in the same manner without modification".It ensures that a derived class does not affect the
+        //behavior of the parent class, in other words,
+        //that a derived class must be substitutable for its base class.
+        static void Main(string[] args)
+        {
+       
+        }
+    }
+}
